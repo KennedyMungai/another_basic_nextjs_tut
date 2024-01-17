e7fabc93edce9b116e701bb07dd13836686abd0a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-17 15:25:54 +0300

    Added teh code to fetch the users

---
+++ app/users/page.tsx
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+type Props = {}
+
+export const fetchUsers = async () => {
+	try {
+		const res = await axios.get(
+			'https://jsonplaceholder.typicode.com/users'
+		)
+		return res.data
+	} catch (error) {
+		throw new Error('Failed to fetch data')
+	}
+}
+
+const UsersPage = async (props: Props) => {
+    const users = await fetchUsers()
+
+	return (
+		<div>
+			<div className=''>
+				{users.map((user: IUser) => (
+					<div key={user.id}>
+						<p>{user.name}</p>
+					</div>
+				))}
+			</div>
+		</div>
+	)
+}
+
+export default UsersPage
