d12031ead3dba45c08de0c7c5263360c986cf706
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-17 15:23:07 +0300

    Remdered out all the users names

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,33 @@
-import Link from "next/link";
+import axios from 'axios'
+import Link from 'next/link'
 
-export default function Home() {
-  return (
-    <main className="h-[90vh] flex flex-col gap-2 justify-start items-center">
-      <h1>Home Page</h1>
-      <p>
-        <Link href={'/users'}>Users</Link>
-      </p>
-    </main>
-  )
+export const fetchUsers = async () => {
+	try {
+		const res = await axios.get(
+			'https://jsonplaceholder.typicode.com/users'
+		)
+		return res.data
+	} catch (error) {
+		throw new Error('Failed to fetch data')
+	}
+}
+
+export default async function Home() {
+	const users = await fetchUsers()
+
+	return (
+		<main className='h-[90vh] flex flex-col gap-2 justify-start items-center'>
+			<h1>Home Page</h1>
+			<p>
+				<Link href={'/users'}>Users</Link>
+			</p>
+			<div className=''>
+				{users.map((user: IUser) => (
+					<div key={user.id}>
+						<p>{user.name}</p>
+					</div>
+				))}
+			</div>
+		</main>
+	)
 }
