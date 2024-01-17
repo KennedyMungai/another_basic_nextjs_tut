9807c4bc96c171f957a92c4837cb2e986052c855
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-17 15:33:34 +0300

    Created the page for an individual user

---
+++ app/users/[userId]/page.tsx
@@ -0,0 +1,39 @@
+import axios from 'axios'
+import React from 'react'
+
+type Props = {
+	params: {
+		userId: string
+	}
+}
+
+export const fetchUser = async (userId: string) => {
+	const res = await axios.get(
+		`https://jsonplaceholder.typicode.com/users/${Number(userId)}`
+	)
+	return res.data
+}
+
+const UserPage = async ({ params: { userId } }: Props) => {
+	const user = await fetchUser(userId)
+
+	return (
+		<div>
+			<h1>{user.name}</h1>
+			<p>{user.email}</p>
+			<p>{user.phone}</p>
+			<p>{user.website}</p>
+			<p>{user.address.city}</p>
+			<p>{user.address.street}</p>
+			<p>{user.address.suite}</p>
+			<p>{user.address.zipcode}</p>
+			<p>{user.address.geo.lat}</p>
+			<p>{user.address.geo.lng}</p>
+			<p>{user.company.name}</p>
+			<p>{user.company.catchPhrase}</p>
+			<p>{user.company.bs}</p>
+		</div>
+	)
+}
+
+export default UserPage
