85347509567eac019ccb0872bc04bebe1af597dd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-17 14:48:57 +0300

    Made some changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,23 +5,23 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'NextJS Refresher',
-  description: 'A NextJS refresher with some new stuff i\'ve not done before',
+	title: 'NextJS Refresher',
+	description: "A NextJS refresher with some new stuff i've not done before"
 }
 
 export default function RootLayout({
-  children,
+	children
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <nav className='pb-5 border-b border-black'>
-          <h1>Some Nav Bar</h1>
-        </nav>
-        {children}
-      </body>
-    </html>
-  )
+	return (
+		<html lang='en' suppressHydrationWarning>
+			<body className={inter.className}>
+				<nav className='pb-5 border-b border-black flex items-center'>
+					<h1>Some Nav Bar</h1>
+				</nav>
+				{children}
+			</body>
+		</html>
+	)
 }
