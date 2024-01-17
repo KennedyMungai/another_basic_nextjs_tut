0e12e53dacce931ae867aa6d8a5282921c096dcc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-17 15:06:42 +0300

    Added some styling for the error page

--- app/about/error.tsx
+++ app/about/error.tsx
@@ -3,7 +3,11 @@
 type Props = {}
 
 const AboutError = (props: Props) => {
-	return <div>AboutError</div>
+	return (
+		<div className='h-[90vh] text-red-500 text-4xl flex items-center justify-center'>
+			Something went Wrong
+		</div>
+	)
 }
 
 export default AboutError
