01dbdf6f6f1ae0d30e85182ab7fe3dc0b41957a8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-17 14:55:47 +0300

    Added a metadata function to the about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import { Metadata } from 'next'
 
 type Props = {}
+
+export const metadata: Metadata = {
+	title: 'About Page',
+	description: 'The about page'
+}
 
 const AboutPage = (props: Props) => {
 	return <div>AboutPage</div>
