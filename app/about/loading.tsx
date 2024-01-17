b4e50fc79056a611ba0bebb2d3495797ce71bf02
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-17 15:02:20 +0300

    Created the loading component for the page

---
+++ app/about/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const AboutLoading = (props: Props) => {
+	return <div className='h-[90vh] flex items-center justify-center text-4xl'>Loading...</div>
+}
+
+export default AboutLoading
