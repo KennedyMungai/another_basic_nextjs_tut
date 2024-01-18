68fecd6c18c452b5d49496e12dceefe51628226e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-18 14:29:01 +0300

    Replaced fetch with axios and added a caching option

--- lib/fetchSingleUserPosts.ts
+++ lib/fetchSingleUserPosts.ts
@@ -1,9 +1,7 @@
-import axios from "axios"
-
 const fetchSingleUserPosts = async (userId: string) => {
     try {
-        const res = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
-        return res.data
+        const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`, { cache: 'force-cache' })
+        return res.json()
     } catch (error: any) {
         throw new Error(error.message)
     }
