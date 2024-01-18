c37be988ac078db2f0b5c4000500a05973f0e70e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-18 14:30:45 +0300

    Did something

--- lib/fetchSingleUserPosts.ts
+++ lib/fetchSingleUserPosts.ts
@@ -1,6 +1,6 @@
 const fetchSingleUserPosts = async (userId: string) => {
     try {
-        const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`, { cache: 'force-cache' })
+        const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`, { next: { revalidate: 60 } })
         return res.json()
     } catch (error: any) {
         throw new Error(error.message)
